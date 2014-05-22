3f1e72fad9ce223b51fecd3ff7a0807cef859b40
Michael Mosmann <michael@mosmann.de>
2014-05-22 15:47:28 +0200

    empty classes

---
+++ src/test/java/codingdojo/FooTest.java
@@ -0,0 +1,16 @@
+package codingdojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	public void test() {
+		boolean thereShouldBeFun=true;
+		
+		assertTrue(thereShouldBeFun);
+	}
+
+}
